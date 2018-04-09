601a0a93a8ec8e6cecde2c6258ac648e3870aec9
Hans Goos <hansgoos@MacBook-Pro-van-Hans.local>
2018-04-09 20:35:27 +0200

    Update build.gradle to include Picasso library

--- GeoTrivia/app/src/main/java/com/example/android/geotrivia/MainActivity.java
+++ GeoTrivia/app/src/main/java/com/example/android/geotrivia/MainActivity.java
@@ -17,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
     public void controleer(View v) {
